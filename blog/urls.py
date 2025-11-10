edd837eb711e0b247ab7d8b554abbf473f6a6655
psatomas <psatomas@gmail.com>
2025-11-10 15:52:23 -0300

    feat: add named URLs and template links for navigation

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="post-page"),
-    path("post/<slug:slug>", views.post_detail, name="post_detail_page")
+    path("post/<slug:slug>", views.post_detail, name="post-detail-page")
 ]
